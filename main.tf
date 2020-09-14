fcc9ebfe611e36813a3ae7ac59b407bf1a1100ea
Imsand <33639785+mimsand@users.noreply.github.com>
2020-09-14 10:55:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,30 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "amanox-tfcloud-002"
-  acl    = "private"
-
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "amanox-tfcloud-003"
+  acl    = "public-read"
+  source = "index.html"
+  website {
+    index_document = var.index_document
+    error_document = var.error_document
+  }
   tags = {
     Name        = "Event"
     Environment = "cncf"
   }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = local.bucket_name
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket       = local.bucket_name
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
 }
