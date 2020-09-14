e41eceef3cb9650596298750480a3eafbd46f4ac
Imsand <33639785+mimsand@users.noreply.github.com>
2020-09-14 11:05:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = local.bucket_name
   acl    = "public-read"
   website {
-    index_document = aws_s3_bucket_object.index
-    error_document = aws_s3_bucket_object.error
+    index_document = "index.html"
+    error_document = "error.html"
   }
   tags = {
     Name        = "Event"
